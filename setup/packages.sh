5f9b7f6379bed2bec2fd5ba9b52622e726a040e4
egmc <waltz4debby@eg2mix.com>
2025-02-02 10:36:41 +0900

    add sudo check

--- setup/packages.sh
+++ setup/packages.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+if [ "$(id -u)" -eq 0 ]; then
+    echo "sudo check ok"
+    echo "----"
+else
+    echo "please run as root user"
+    exit
+fi
+
+
 apt update
 apt install -y bpftrace
 
