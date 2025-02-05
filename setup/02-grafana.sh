d264c9663a56b1819d3e7e4687b8c8bc113be380
egmc <waltz4debby@eg2mix.com>
2025-02-05 20:11:48 +0900

    grafana installation

--- setup/02-grafana.sh
+++ setup/02-grafana.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# ref
+# https://grafana.com/docs/grafana/latest/setup-grafana/installation/debian/
+
+
 if [ "$(id -u)" -eq 0 ]; then
     echo "sudo check ok"
     echo "----"
@@ -7,3 +11,10 @@ else
     echo "please run as root user"
     exit
 fi
+DEBIAN_FRONTEND=noninteractive sudo apt-get install -y apt-transport-https software-properties-common wget
+mkdir -p /etc/apt/keyrings/
+wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | tee /etc/apt/keyrings/grafana.gpg > /dev/null
+
+echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+apt-get update
+apt-get install grafana
