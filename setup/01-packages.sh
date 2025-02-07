ea9ae4f61d559f15460a9533478bf2c2e02667d4
sohei.iwahori <sohei.iwahori@gree.net>
2025-02-07 18:03:34 +0900

    remove mysql server to reduce memory consumptiopn

--- setup/01-packages.sh
+++ setup/01-packages.sh
@@ -35,7 +35,6 @@ apt-get install -y python3-pip
 # middlewares
 
 apt install -y apache2
-apt install -y mysql-server
 apt install -y memcached
 apt install -y php
 apt install -y php-curl php-dev php-gd php-xml php-yaml php-mbstring php-mysqlnd php-memcached
